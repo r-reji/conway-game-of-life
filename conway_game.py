e0a99be5d2015d580688036fe0358a3b1bb1a4c0
r-reji <rj.rebin@gmail.com>
2023-02-22 15:45:01 +0000

    project outline

--- conway_game.py
+++ conway_game.py
@@ -1,2 +1,20 @@
 import tkinter
 
+# * GOAL: Simulate Conway's Game of life with OOP
+
+# TODO: class method to initialise GUI
+# TODO: class method to build game grid
+# TODO: class method for per cell toggle
+# TODO: class method to simulate game
+# TODO: class method for randomised initial state
+# TODO: GUI buttons for start/stop/randomise/generations
+# TODO: class method for reset
+
+class game:
+
+    def __init__(self):
+        pass
+
+if __name__ == '__main__':
+    game_of_life = game()
+    
